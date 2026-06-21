8e39634d1e44116b283dc31e1e012b54f590c6e2
darshanbhavsar01 <darshanbhavsar4.db@gmail.com>
2026-06-21 19:10:01 +0530

    changed model path

--- travis_app.py
+++ travis_app.py
@@ -112,7 +112,7 @@ st.markdown("""
 # ─────────────────────────────────────────────────────────────────────
 # LOAD MODELS & LOOKUPS  (cached)
 # ─────────────────────────────────────────────────────────────────────
-MODEL_DIR = r"/models"
+MODEL_DIR = "/models"
 
 @st.cache_resource
 def load_models():
